26060b01abfb1562a83548578274c9f6b16fc715
Poiati <poiatipts@gmail.com>
2021-07-17 16:50:04 -0300

    Criando um Controller Olá Mundo!

---
+++ src/main/java/com/eventoapp/eventoapp/indexController.java
@@ -0,0 +1,13 @@
+package com.eventoapp.eventoapp;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class indexController {
+	
+	public String index()
+	{
+		return "index";
+	}
+
+}
