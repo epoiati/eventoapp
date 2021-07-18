03e1c993298c9b3bc5b142ec2d64037a0ab688c6
Poiati <poiatipts@gmail.com>
2021-07-18 10:04:12 -0300

    cotinuacão

--- src/main/java/com/eventoapp/eventoapp/EventoController.java
+++ src/main/java/com/eventoapp/eventoapp/EventoController.java
@@ -2,18 +2,27 @@ package com.eventoapp.eventoapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.eventoapp.models.Convidado;
 import com.eventoapp.models.Evento;
+import com.eventoapp.repository.ConvidadoRepository;
 import com.eventoapp.repository.EventoRepository;
 
 @Controller
 public class EventoController {
 	
+	
+	
 	@Autowired
 	private EventoRepository er;
+	
+	@Autowired
+	private ConvidadoRepository cr;
+	
 	
 	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.GET)
 	public String form(){
@@ -41,6 +50,23 @@ public class EventoController {
 		
 	}
 	
+	@RequestMapping(value="/{codigo}", method=RequestMethod.GET)
+	public ModelAndView detalhesEvento(@PathVariable("codigo") long codigo){
+		Evento evento = er.findByCodigo(codigo);
+		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
+		return mv.addObject("evento", evento);
+	}
+	
+	
+	@RequestMapping(value="/{codigo}", method=RequestMethod.POST)
+	public String detalhesEvento(@PathVariable("codigo") long codigo, Convidado conviadado){
+		Evento evento = er.findByCodigo(codigo);
+		convidado.set;
+	
+		return "redirect:/(codigo)";
+	}
+	
+		
 	
 	
 	
--- src/main/java/com/eventoapp/models/Convidado.java
+++ src/main/java/com/eventoapp/models/Convidado.java
@@ -1,9 +1,11 @@
 package com.eventoapp.models;
 
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+@SuppressWarnings("unused")
 @Entity
 public class Convidado {
 
